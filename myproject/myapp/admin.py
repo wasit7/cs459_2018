cd3b97b5d3e52391cbd9637420b9bbdb986733aa
Wasit Limprasert <wasit7@gmail.com>
2019-02-13 14:08:30 +0700

    init week05

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -3,5 +3,8 @@ from .models import Product
 
 class ProductAdmin(admin.ModelAdmin):
     fields = ('name', 'price', 'category', 'image')
+    list_display = ('name', 'price', 'category', 'image')
+    list_filter = ('category', 'price', )
+    list_editable = ('price', 'category', 'image', )
 
 admin.site.register(Product, ProductAdmin)
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,21 +3,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
 from .models import Product
+from django.views import generic
 
 def current_datetime(request):
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
     return HttpResponse(html)
 
-# def product_list(request):
-#     text="""
-#         <ul>
-#     """
-#     for i in Product.objects.all():
-#         text = text+'<li>'+str( i )+'</li>'
-#     return HttpResponse(text)
-
-
 def  product_list(request):
     context={'key':'value','products':Product.objects.all()}
     return render(request, 'product_list.html', context)
+
+
